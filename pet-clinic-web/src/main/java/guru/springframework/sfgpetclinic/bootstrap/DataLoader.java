fec94fcf143f7e8a81fbcedf0ee460ca400fb2a0
Hanen Benali <hanen.benali@sofrecom.com>
2022-07-19 16:59:28 +0100

    Load Bootstrap data on startup Closes #15

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -0,0 +1,59 @@
+package guru.springframework.sfgpetclinic.bootstrap;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.VetService;
+import guru.springframework.sfgpetclinic.services.map.OwnerServiceMap;
+import guru.springframework.sfgpetclinic.services.map.VetServiceMap;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final OwnerService ownerService;
+    private final VetService vetService;
+    public DataLoader(){
+        this.ownerService = new OwnerServiceMap();
+
+        this.vetService = new VetServiceMap();
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Owner o1 = new Owner();
+        o1.setId(1L);
+        o1.setFirstName("Michael");
+        o1.setLastName("Weston");
+
+        ownerService.save(o1);
+
+        Owner o2 = new Owner();
+        o2.setId(2L);
+        o2.setFirstName("Fiona");
+        o2.setLastName("Glenanne");
+
+        ownerService.save(o2);
+
+        System.out.println("Loaded owners......");
+
+        Vet v1 = new Vet();
+        v1.setId(1L);
+        v1.setFirstName("Sam");
+        v1.setLastName("Axe");
+
+        vetService.save(v1);
+
+        Vet v2 = new Vet();
+        v2.setId(2L);
+        v2.setFirstName("Jessie");
+        v2.setLastName("Porter");
+
+        vetService.save(v2);
+
+        System.out.println("loaded Vets..........");
+
+    }
+}
