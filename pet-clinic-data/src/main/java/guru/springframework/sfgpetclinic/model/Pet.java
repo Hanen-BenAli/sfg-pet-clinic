e3aec0ff1a7300361d636a3a3945b7a70522095e
Hanen Benali <hanen.benali@sofrecom.com>
2022-07-21 23:14:49 +0100

    Implement Visit JPA. Closes #39

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -22,4 +23,7 @@ public class Pet extends BaseEntity{
     private Owner owner;
     @Column(name = "birthday")
     private LocalDate birthday;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits;
 }
